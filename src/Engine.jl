b340a88ce106994256f8ad5ff1564e3b867b0717
thiam0412 <aliounebadarathiam17@gmail.com>
2025-12-15 00:30:34 +0100

    Update Engine.jl

--- src/Engine.jl
+++ src/Engine.jl
@@ -8,12 +8,14 @@ en utilisant le calcul de la limite.
 input : f à dériver
 output : fp = df/dx
 """
+
 function prime(f)
     h = 0.00000001
     function fp(x)
         (f(x+h)-f(x))/h
     end  
-return fp
+return fp 
 end
 end 
+
 
